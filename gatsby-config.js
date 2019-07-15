3f455ba472c4770a97f44cca23e0e1fa658b855e
berkeleycole <ahope@ahopemindgruvenet.local>
2019-07-15 09:34:18 -0700

    the right configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 let contentfulConfig
 
 contentfulConfig = {
-	spaceId: process.env.wqeegw4zw7wf || contentfulConfig.spaceId,
-	accessToken: process.env.nNMYKX7W_khEKjAE9-Kouj4B8-_F0WhsT7yY3DVG35c || contentfulConfig.accessToken,
-	host: process.env.preview.contenful.com
+	spaceId: process.env.CONTENFUL_SPACE_ID || contentfulConfig.spaceId,
+	accessToken: process.env.CONTENFUL_PREVIEW_ACCESS_TOKEN || contentfulConfig.accessToken,
+	host: process.env.preview.CONTENTFUL_HOST
 }
 
 try {
